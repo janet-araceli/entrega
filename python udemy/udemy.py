bdd25b347fe7ea182e0a4f66dd07eb6f19c5d4c0
janet-araceli <128119435+janet-araceli@users.noreply.github.com>
2023-09-20 19:59:04 -0600

    Add files via upload

---
+++ python udemy/udemy.py
@@ -0,0 +1,5 @@
+
+nombre = input("ingrese su nombre")
+haorro_semanal = int(input("ingrese cuanto haorraras ala semana"))
+haorro_futuro = haorro_semanal * 10
+print("hola mi querid@", nombre, ", en 10 semanas tendras", haorro_futuro, "haorrado." )
